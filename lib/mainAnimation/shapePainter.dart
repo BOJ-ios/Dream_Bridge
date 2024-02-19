4848c27c035c46166af2b1e94bef510142c83068
Eunbyul <89649741+EunByu1@users.noreply.github.com>
2024-02-20 02:05:48 +0900

    Add Design

--- lib/mainAnimation/shapePainter.dart
+++ lib/mainAnimation/shapePainter.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 
-import 'animatedShape.dart';
-
+import 'package:dream_bridge/mainAnimation/shape.dart';
 
 class ShapePainter extends CustomPainter {
   List<AnimatedShape> shapes;
+  double progress;
 
-  ShapePainter(this.shapes);
+  ShapePainter(this.shapes, this.progress);
 
   @override
   void paint(Canvas canvas, Size size) {
     for (var shape in shapes) {
       final paint = Paint()..color = Colors.purple.withAlpha(150);
+      shape.move(progress);
+
       switch (shape.shapeType) {
         case ShapeType.circle:
           canvas.drawCircle(shape.position, shape.size, paint);
@@ -22,9 +24,9 @@ class ShapePainter extends CustomPainter {
           break;
         case ShapeType.triangle:
           final path = Path();
-          path.moveTo(shape.position.dx, shape.position.dy - shape.size);
-          path.lineTo(shape.position.dx - shape.size, shape.position.dy + shape.size);
-          path.lineTo(shape.position.dx + shape.size, shape.position.dy + shape.size);
+          path.moveTo(shape.position.dx, shape.position.dy - shape.size / 2);
+          path.lineTo(shape.position.dx - shape.size / 2, shape.position.dy + shape.size / 2);
+          path.lineTo(shape.position.dx + shape.size / 2, shape.position.dy + shape.size / 2);
           path.close();
           canvas.drawPath(path, paint);
           break;
